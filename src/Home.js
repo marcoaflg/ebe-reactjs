6d7c9b35e481c1511230782ec442b3e7adffe980
Marco Garcia <marcosl19@gmail.com>
2020-05-06 00:58:21 -0300

    add components and images

---
+++ src/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Home.css';
+import Nav from './components/Nav'
+
+function Home() {
+  return (
+    <div className="Home">
+      <>
+        <Nav />
+      </>
+    </div>
+  );
+}
+
+export default Home;
